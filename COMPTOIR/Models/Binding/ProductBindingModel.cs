cec48265510a61f5739a666de23a3ca6eca1ce73
amrkarouni <amrkarouni@gmail.com>
2022-12-23 19:44:16 +0300

    fix product binding model

--- COMPTOIR/Models/Binding/ProductBindingModel.cs
+++ COMPTOIR/Models/Binding/ProductBindingModel.cs
@@ -2,18 +2,18 @@
 {
     public class ProductBindingModel
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Code { get; set; }
         public string? Manifacturer { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
-        public bool? IsFinal { get; set; } = true;
-        public bool? IsRaw { get; set; } = false;
-        public DateTime? CreatedDate { get; set; } = DateTime.UtcNow;
+        public bool IsFinal { get; set; } = true;
+        public bool IsRaw { get; set; } = false;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
         public string? UnitName { get; set; }
-        public int? SubCategoryId { get; set; }
-        public bool? IsDeleted { get; set; } = false;
+        public int SubCategoryId { get; set; }
+        public bool IsDeleted { get; set; } = false;
         public double? Price { get; set; } = 0;
 
     }
