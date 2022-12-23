a33a01bac4d62c0c2d5489d08950a25a5dc74a6a
amrkarouni <amrkarouni@gmail.com>
2022-12-23 19:42:25 +0300

    product binding model

--- COMPTOIR/Models/Binding/ProductBindingModel.cs
+++ COMPTOIR/Models/Binding/ProductBindingModel.cs
@@ -2,7 +2,7 @@
 {
     public class ProductBindingModel
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string? Name { get; set; }
         public string? Code { get; set; }
         public string? Manifacturer { get; set; }
